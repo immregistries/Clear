deccfdf39dbb84e1bcc1e6975afeb2b65bb2df18
Nathan Bunker <nbunker@immregistries.org>
2025-01-31 17:30:13 -0700

    Initial commit, port from Step

---
+++ src/main/java/org/immregistries/clear/SoftwareVersion.java
@@ -0,0 +1,5 @@
+package org.immregistries.clear;
+
+public class SoftwareVersion {
+  public static final String VERSION = "0.1";
+}
---
+++ src/main/java/org/immregistries/clear/servlet/maps/MapEntityType.java
@@ -0,0 +1,6 @@
+package org.immregistries.clear.servlet.maps;
+
+public enum MapEntityType {
+    PATH,
+    CIRCLE
+}
